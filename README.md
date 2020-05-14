654cd2a98318036ca14d24c6efee18d86f765a26
chaifanfan <yf2958@163.com>
2020-05-15 03:32:05 +0800

    Scroll Sketch

--- README.md
+++
@@ -1,3 +0,0 @@
-# Landing Page
-
-Landing Page is a project fot Breiter. It will be carefully build with many modules.
---
+++ js/app.js
@@ -0,0 +1,4 @@
+function switchBlock() {
+  document.getElementsByClassName('block-one')[0].classList.add('hidden');
+  document.getElementsByClassName('block-two')[0].classList.remove('hidden');
+}
