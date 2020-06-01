939853be054c1bfceea41698f7300b7ec1eb548a
chaifanfan <yf2958@163.com>
2020-06-02 07:51:50 +0800

    Add size option

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ function switchBlock() {
   document.getElementsByClassName('block-one')[0].classList.add('hidden');
   document.getElementsByClassName('block-two')[0].classList.remove('hidden');
 }
+
+function selectSize(obj) {
+  let buttons = document.querySelectorAll('.size-option button')
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('selected')
+  }
+  obj.classList.add('selected')
+}
